b640ed2511e33298473a2ecf58d652bbeebce88e
unknown <ccamarillo32298@gmail.com>
2020-02-02 18:34:17 -0500

    created parser for datasets and created unweighted knn

--- knn.py
+++ knn.py
@@ -11,18 +11,59 @@ def read_data(name):
 
     for i in range(len(data)):
         data[i] = data[i].split(",")
+        data[i] = parse(data[i])
 
     return data
 
 # T = training set, x = unknown instance
-def knn(T, x):
-    
+''' The last element of an example is assumed to be its class
+    therefore x's last element should be ommited
+'''
 
+def knn(T, x, k, weighted=False):
+    # TODO: normalize T such that each attribute is between 0-1
+    # TODO: implement weighted
+    
+    # obtain possible neighbors by (class, distance)
+    # and keep K nearest
+    neighbors = [(ex[-1], distance(x, ex)) for ex in T]
+    neighbors.sort(key = lambda ex: ex[1])
+    neighbors = neighbors[:k]
+    
+    #DEBUG CODE
+    print(neighbors)
+    
+    
+def distance(x, ex):
+    diffs_squared = []
+
+    # if it is a discrete difference count them by 1
+    for i in range(len(x)):
+        if type(x[i]) is str:
+            diffs_squared.append(1 if x[i] != ex[i] else 0)
+        else:
+            diffs_squared.append((ex[i] - x[i]) ** 2)
+    
+    return math.sqrt(sum(diffs_squared))
+
+
+# convert strings into types
+def parse(attributes):
+    new_vector = []
+    for a in attributes:
+        try:
+            new_vector.append(float(a))
+        except ValueError:
+            new_vector.append(a)
+    return new_vector
 
 if __name__ == "__main__":
     abalone = "abalone.data"
     iris = "iris.data"
     iris_data = read_data(iris)
+    abalone_data = read_data(abalone)
 
-    for i in range(len(iris_data)):
-        print(iris_data[i])
+    knn(iris_data, iris_data[2], 3)
+    
+    
+    
